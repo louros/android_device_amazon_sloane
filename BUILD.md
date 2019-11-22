f0bb7aaa5bb7f3df425b633bc4647d50de692eea
Roger Ortiz <rogerortizleal@gmail.com>
2019-11-22 17:29:07 +0100

    sloane: Add build script.

--- BUILD.md
+++ BUILD.md
@@ -1,4 +1,5 @@
 # Build manual
+
 * Download the sources
 ```
 $ repo init -u https://github.com/cm12-amami/android.git -b cm-12.1 --groups=all,-notdefault,-darwin,-x86,-mips
@@ -11,10 +12,10 @@ $ repo sync --no-tags
 $ git clone https://github.com/R0rt1z2/android_device_amazon_sloane.git device/amazon/sloane
 $ git clone https://github.com/R0rt1z2/android_vendor_amazon_sloane.git vendor/amazon/sloane
 ```
+
 * Start building
 ```
-$ source ./build/envsetup.sh
-$ lunch cm_sloane-userdebug
-$ LANG=C mka bacon -j8
+$ chmod a+x device/amazon/sloane/build.sh && ./device/amazon/sloane/build.sh
 ```
+
 * Flash the ROM + SuperSU
---
+++ build.sh
@@ -0,0 +1,45 @@
+#bin/bash
+write=echo
+
+var=userdebug
+device=sloane
+
+y=$(date +%Y)
+m=$(date +%m)
+d=$(date +%d)
+fulld=$y$m$d
+
+zip=$(pwd)/out/target/product/$device/lineage-12.1-$fulld-UNOFFICIAL-$device.zip
+
+$write " _ _                                             "
+$write "| (_)_ __   ___  __ _  __ _  ___  ___  ___       "
+$write "| | | '_ \\ / _ \\/ _\` |/ _\` |/ _ \\/ _ \\/ __|"
+$write "| | | | | |  __/ (_| | (_| |  __/ (_) \\__ \\    "
+$write "|_|_|_| |_|\\___|\\__,_|\\__, |\\___|\\___/|___/ "
+$write "                      |___/                      "
+
+echo ""
+
+echo "Setting a few variables.."
+export LANG=C
+
+echo "Preparing environment..."
+cd .. && cd .. && cd ..
+source ./build/envsetup.sh > /dev/null 2>&1
+
+echo "Prepare for cook time..."
+lunch cm_$device-$var > /dev/null 2>&1
+
+read -p "Cleanup old files? y/n >> " clean
+if [ $clean = "y" ]; then
+   echo "Cleaning up..."
+   make clobber > /dev/null 2>&1
+   make clean > /dev/null 2>&1
+else
+   echo "Not cleaning up..."
+fi
+
+echo "Start the build..."
+mka bacon -j8 | tee build.log
+echo "Done! ROM ZIP FILE: $zip"
+
