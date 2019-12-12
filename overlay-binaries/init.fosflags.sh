d9197e169563dc0baad1678c850c51f4789cb4e2
Roger Ortiz <39171306+R0rt1z2@users.noreply.github.com>
2019-12-12 22:42:56 +0100

    init.fosflags.sh: Open Wi-Fi Direct Settings insted of install certificates...

--- overlay-binaries/init.fosflags.sh
+++ overlay-binaries/init.fosflags.sh
@@ -42,6 +42,6 @@ su -c "svc wifi disable"
 su -c "svc wifi enable"
 am start -a android.settings.WIFI_IP_SETTINGS
 sleep 3
-input tap 223 850
+input tap 223 950
 sleep 20 # Here user presses home button 15-20 secs
 input tap 223 300 # Simulate touch to connect the remote
