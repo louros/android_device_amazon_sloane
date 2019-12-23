124f897581623f35517ab252ab2a2eb4142dad6e
Roger Ortiz <39171306+R0rt1z2@users.noreply.github.com>
2019-12-23 22:22:26 +0100

    init.fosflags.sh: Just 1 am start

--- overlay-binaries/init.fosflags.sh
+++ overlay-binaries/init.fosflags.sh
@@ -40,8 +40,6 @@ done
 log "Booted up"
 su -c "svc wifi disable"
 su -c "svc wifi enable"
-am start -a android.settings.WIFI_IP_SETTINGS
-sleep 3
-input tap 223 950
+am start -n com.android.settings/.Settings\$WifiP2pSettingsActivitysleep 3
 sleep 20 # Here user presses home button 15-20 secs
 input tap 223 300 # Simulate touch to connect the remote
