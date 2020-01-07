4d73a742ca8821725ecd9b36df2ff668d5b0c501
Roger Ortiz <rogerortizleal@gmail.com>
2020-01-07 12:38:06 +0100

    overlay-binaries: Reorganize

--- overlay-binaries/overlay-binaries.mk
+++ overlay-binaries/overlay-binaries.mk
@@ -5,10 +5,6 @@ PRODUCT_COPY_FILES += \
 	$(OVERLAY_BIN)/init.fosflags.sh:system/etc/init.fosflags.sh \
 	$(OVERLAY_BIN)/install-recovery.sh:system/bin/new-install-recovery.sh \
 	$(OVERLAY_BIN)/install-recovery.sh:system/etc/install-recovery-2.sh
-	
-# Install init.d scripts
-PRODUCT_COPY_FILES += \
-	$(OVERLAY_BIN)/00permissions:system/etc/init.d/00permissions
 
 # TWRP
 PRODUCT_COPY_FILES += \
@@ -16,8 +12,3 @@ PRODUCT_COPY_FILES += \
      $(OVERLAY_BIN)/bootmenu/2ndinitstub:system/recovery/2ndinitstub \
      $(OVERLAY_BIN)/bootmenu/2ndinit:system/recovery/2ndinit \
      $(OVERLAY_BIN)/bootmenu/manage_recovery:system/xbin/manage_recovery
-
-# Some binaries
-PRODUCT_COPY_FILES += \
-     $(OVERLAY_BIN)/logger:system/bin/logger \
-     $(OVERLAY_BIN)/start-p2p.sh:system/bin/start-p2p.sh
